d9524657a5f8cee6974cf6b00f77f34994aceef4
Grant Hendricks <gtnhen@gmail.com>
2025-02-24 03:09:27 -0500

    styles & scripts

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 70,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+let navbar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.95)';
+    } else {
+        navbar.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+    }
+});
